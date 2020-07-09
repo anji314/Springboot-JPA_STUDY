cf7094ad185a50e02ad4489479bb0ba00062d4d3
Anjihye <anji314@naver.com>
2020-07-09 17:29:06 +0900

    exercise 데이터베이스 스키마 자동생성

--- MappingTest2/src/main/java/hellojpa/Member.java
+++ MappingTest2/src/main/java/hellojpa/Member.java
@@ -1,9 +1,7 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 //Table(name="MEMBER")
@@ -18,6 +16,19 @@ public class Member {
 
     private Integer age;
 
+    //== 추가 ==
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private  String description;
+
     public String getId(){return id;}
     public void setId(String id){this.id=id;}
 
@@ -26,4 +37,40 @@ public class Member {
 
     public Integer getAge(){return age;}
     public void setAge(Integer age){this.age=age;}
+
+    public RoleType getRoleType() {
+        return roleType;
+    }
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
+
+
+enum RoleType{
+    ADMIN,USER
 }
